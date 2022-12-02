f40d70f47e5b23bcaef3f4aa2f733379bf1ed8ea
jfong <j.fongli@mail.utoronto.ca>
2022-12-01 19:46:53 -0500

    WindowUI now has a title.

--- src/main/java/dialogue/DialogueRunTest.java
+++ src/main/java/dialogue/DialogueRunTest.java
@@ -12,5 +12,6 @@ public class DialogueRunTest {
 //    public DialogueRunTest() {
 //        GameWindowUI.createWindow();}
 
+
 }
 
--- src/main/java/dialogue/GameWindowUI.java
+++ src/main/java/dialogue/GameWindowUI.java
@@ -9,6 +9,8 @@ Creates the window for "Game".
 
 public class GameWindowUI {
 
+    Font titleFont = new Font("Papyrus", Font.PLAIN, 80);
+
 //    public GameWindowUI() { }
 
     public void createWindow() {
@@ -19,6 +21,19 @@ public class GameWindowUI {
         window.getContentPane().setBackground(Color.black);
         window.setLayout(null);
 
+        // TITLE SCREEN PANEL
+        JPanel titlePanel = new JPanel();
+        titlePanel.setBounds(100, 100, 600, 150);
+        titlePanel.setBackground(Color.blue);
+
+        // TITLE TEXT
+        JLabel titleNameLabel = new JLabel("STUDY HERO");
+        titleNameLabel.setForeground(Color.white);
+        titleNameLabel.setFont(titleFont);
+        titlePanel.add(titleNameLabel);
+
+
+        window.add(titlePanel);
         window.setVisible(true);
 
     }
