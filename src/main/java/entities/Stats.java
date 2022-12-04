33f9c2f2cae9fe2547cc79e435697576a1fb28ec
jahink17 <jahinkabir17@gmail.com>
2022-12-04 18:54:39 -0500

    Modified Stats interface and StatsUser class to contain new instance attributes tempDamage, baselineDamage, currHp and maxHp (these are replacing hp and damage). Made Boss interface and implemented it with BossClass. Made StatsUpdateInteractor which accesses creates new Stats object from memory and then updates it and saves it back to memory.

--- src/main/java/entities/Stats.java
+++ src/main/java/entities/Stats.java
@@ -9,37 +9,40 @@ public interface Stats {
     /** Returns a map containing the type of stats (i.e. level, gold, hp, damage, defence or bosshp) as the key and its
      * amount as the value.
      */
-    public Map<String, Integer> getUserStats();
+    Map<String, Integer> getUserStats();
 
     /** Changes the amount of gold the player has and returns the updated amount.
      *
      * @param by amount to change the gold by
      * @return new amount of gold
      */
-    public int changeGold(int by);
+    int updateGold(int by);
+    int getTempDamage();
 
     /** Increases the player's level by 1 and returns the new level
      *
      * @return new level
      */
-    public int updateLevel();
+    int updateLevel();
 
     /** Changes the amount of damage that can be done by the user and returns the new value.
      * @param by amount to change by
      * @return new damage value
      */
-    public int changeDamage(int by);
+    int updateBaselineDamage(int by);
+    int updateTempDamage(int by);
+    int resetTempDamage();
 
     /** Changes the amount of hp that the user has and returns the new value.
      * @param by amount to change by
      * @return new hp value
      */
-    public int changeHP(int by);
+    int updateCurrHp(int by);
 
     /** Changes the ability of the user to defend themselves and returns the new value.
      * @param by amount to change by
      * @return new defence value
      */
-    public int changeDefence(int by);
+    int updateDefence(int by);
 
 }
