ed62bd3916b8b955b6f9077cf9981c91775dbefa
Victor <yvictor2016@gmail.com>
2022-12-06 12:50:35 -0500

    Moved action listeners for each element on the UI into their own classes

--- src/main/java/Timer/timer_screen/CustomTimerConfirmButtonListener.java
+++ src/main/java/Timer/timer_screen/CustomTimerConfirmButtonListener.java
@@ -1,21 +1,41 @@
 package Timer.timer_screen;
 
-import Timer.timer_use_cases.TimerRequestModel;
-
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Pattern;
 
 public class CustomTimerConfirmButtonListener implements ActionListener {
-    private TimerRequestModel tRequestModel;
-    private PresetTimerController ptController;
     private CustomTimerController ctController;
-    private CustomTimerTextField customTimerTextField;
+    private JFormattedTextField customTimerTextField;
     private JLabel timerText;
     private int totalTime;
+    private JProgressBar progressBar;
+    private JLabel errorLabel;
+
+    public CustomTimerConfirmButtonListener(CustomTimerController ctController, JFormattedTextField customTimerTextField, JLabel timerText, JProgressBar progressBar, JLabel errorLabel) {
+        this.ctController = ctController;
+        this.customTimerTextField = customTimerTextField;
+        this.timerText = timerText;
+        this.progressBar = progressBar;
+        this.errorLabel = errorLabel;
+    }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        String inputTime = customTimerTextField.getText();
+        if (Pattern.matches("^[0-9]{2}:[0-5][0-9]:[0-5][0-9]$", inputTime)) {
+            ctController.setCustomTime(inputTime);
+            timerText.setText(inputTime);
+            int totalSeconds = TimerPanel.convertTimeToInt(inputTime);
+            progressBar.setMaximum(totalSeconds);
+            errorLabel.setText("");
+        }
+        else {
+            errorLabel.setForeground(Color.RED);
+            errorLabel.setText("Invalid Time");
+        }
     }
 }
--- src/main/java/Timer/timer_screen/PresetShortButtonListener.java
+++ src/main/java/Timer/timer_screen/PresetShortButtonListener.java
@@ -1,10 +1,28 @@
 package Timer.timer_screen;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PresetShortButtonListener implements ActionListener {
-    public void actionPerformed(ActionEvent e) {
+    private PresetTimerController ptController;
+    private CustomTimerController ctController;
+    private JLabel timerText;
+    private JProgressBar progressBar;
 
+    public PresetShortButtonListener(PresetTimerController ptController, CustomTimerController ctController, JLabel timerText, JProgressBar progressBar) {
+        this.ptController = ptController;
+        this.ctController = ctController;
+        this.timerText = timerText;
+        this.progressBar = progressBar;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        ctController.setCustomTime("-1");
+        ptController.selectShortTime();
+        String time = ptController.getShortTime();
+        timerText.setText(time);
+        int totalSeconds = TimerPanel.convertTimeToInt(time);
+        progressBar.setMaximum(totalSeconds);
     }
 }
