c56bbcbe02e8a1930d60d0fbdcc7775b49cb4269
Cana <cana.leung@mail.utoronto.ca>
2022-12-08 12:14:54 -0500

    Update new boss fight files into GUI branch

--- src/main/java/entities/MusicPlayer.java
+++ src/main/java/entities/MusicPlayer.java
@@ -1,30 +1,27 @@
 package entities;
 
-import use_cases.errors.ErrorOutputBoundary;
+import use_cases.errors.ErrorPresenter;
 
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 
 public class MusicPlayer {
-    private Long currentPos;
-    private Clip clip;
+    Long currentPos;
+    Clip clip;
     String status;
     AudioInputStream inputStream;
     String filepath;
-    ErrorOutputBoundary presenter;
+    ErrorPresenter presenter;
 
-    public MusicPlayer(String filepath, ErrorOutputBoundary presenter){
-        this.presenter = presenter;
-
+    public MusicPlayer(String filepath){
+        this.presenter = new ErrorPresenter();
         try {
             this.filepath = filepath;
             this.inputStream = AudioSystem.getAudioInputStream(new File(filepath));
             this.clip = AudioSystem.getClip();
             clip.open(inputStream);
             clip.loop(Clip.LOOP_CONTINUOUSLY);
-            this.play();
-            this.status = "play";
 
         } catch (UnsupportedAudioFileException | LineUnavailableException
                  | IOException e) {
@@ -32,16 +29,25 @@ public class MusicPlayer {
         }
     }
 
-    public String getStatus(){return status;}
-    public void setStatus(String newStatus){status = newStatus;}
-    public Long getCurrentPos(){return currentPos;}
-    public void setCurrentPos(Long Pos){currentPos = Pos;}
-    public Clip getClip(){return clip;}
-
-
     public void play(){
         clip.start();
         status = "play";
+    }
+    public void pause(){
+        if (!status.equals("paused")){
+            this.currentPos = this.clip.getMicrosecondPosition();
+            clip.stop();
+            status = "paused";
+        }
+    }
+
+    public void restart(){
+        clip.stop();
+        clip.close();
+        resetAudioStream();
+        currentPos = 0L;
+        clip.setMicrosecondPosition(0);
+        this.play();
     }
 
     public void resetAudioStream()
@@ -56,5 +62,6 @@ public class MusicPlayer {
             throw new RuntimeException(e);
         }
     }
+
 
 }
