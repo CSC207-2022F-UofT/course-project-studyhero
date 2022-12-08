38504760b8d41d585589e0b6c36b7a38a06adab0
Victor <yvictor2016@gmail.com>
2022-12-07 20:41:04 -0500

    Added FocusListener to CustomTimerTextField Made buttons unclickable once timer is started

--- src/main/java/UI/screens/timer_screen/CustomTimerTextFieldFocusListener.java
+++ src/main/java/UI/screens/timer_screen/CustomTimerTextFieldFocusListener.java
@@ -5,6 +5,7 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
 public class CustomTimerTextFieldFocusListener implements FocusListener {
+
     public CustomTimerTextFieldFocusListener(JFormattedTextField customTimerTextField) {
         this.customTimerTextField = customTimerTextField;
     }
