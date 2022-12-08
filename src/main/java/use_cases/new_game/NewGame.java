feb0e5cbfba5e789d3b6eaee038b6f19f09d6895
Cana <cana.leung@mail.utoronto.ca>
2022-12-08 11:42:20 -0500

    Created FightingStatsIntialiser and edited StatsSave implementation

--- src/main/java/use_cases/new_game/NewGame.java
+++ src/main/java/use_cases/new_game/NewGame.java
@@ -2,10 +2,8 @@ package use_cases.new_game;
 
 import entities.FightingStatsInitializer;
 import entities.StatsUser;
-import entities.inventoryitem.InitializePlayerInventoryGateway;
-import entities.inventoryitem.InitializeShopInventoryGateway;
-import entities.inventoryitem.PlayerInventoryFile;
-import entities.inventoryitem.ShopInventoryFile;
+import inventorymenu.inventoryitem.InventoryList;
+import inventorymenu.inventoryitem.PlayerInventoryFile;
 import use_cases.errors.ErrorOutputBoundary;
 import use_cases.file_checker.ValidFileDsGateway;
 import use_cases.save_game.StatSave;
@@ -15,29 +13,27 @@ import java.util.Map;
 
 public class NewGame implements NewGameInputBoundary{
     private final ValidFileDsGateway statsFile;
-    private final ValidFileDsGateway playerInventoryFile;
-    private final ValidFileDsGateway shopInventoryFile;
+    private final ValidFileDsGateway inventoryFile;
     private final ValidFileDsGateway fightStatsFile;
     private final ErrorOutputBoundary presenter;
 
     public NewGame(ValidFileDsGateway statsFile,
-                   ValidFileDsGateway playerInventoryFile,
-                   ValidFileDsGateway shopInventoryFile, ValidFileDsGateway fightStatsFile,
+                   ValidFileDsGateway inventoryFile,
+                   ValidFileDsGateway fightStatsFile,
                    ErrorOutputBoundary presenter) {
         this.statsFile = statsFile;
-        this.playerInventoryFile = playerInventoryFile;
-        this.shopInventoryFile = shopInventoryFile;
+        this.inventoryFile = inventoryFile;
         this.fightStatsFile = fightStatsFile;
         this.presenter = presenter;
     }
 
     public boolean isPlayable(){
 
-        return (statsFile.isPlayable() && playerInventoryFile.isPlayable());
+        return (statsFile.isPlayable() && inventoryFile.isPlayable());
     }
 
     public void newGame(){
-        if (statsFile.fileExists() && playerInventoryFile.fileExists()
+        if (statsFile.fileExists() && inventoryFile.fileExists()
                 && fightStatsFile.fileExists()){
             System.out.println("Files exist. Overwriting...");
         }else{
@@ -48,13 +44,9 @@ public class NewGame implements NewGameInputBoundary{
         StatSave newSave = new StatSave(newUser.getUserStats(), presenter);
         newSave.save("stats.csv");
 
-        // initialise a new player inventory list and saves it
-        InitializePlayerInventoryGateway playerInventoryList = new PlayerInventoryFile("PlayerInventory.csv");
-        playerInventoryList.initialize();
-
-        // initialise a new shop inventory list and saves it
-        InitializeShopInventoryGateway shopInventoryList = new ShopInventoryFile("ShopInventory.csv");
-        shopInventoryList.initialize();
+        // initialise a new inventory list and saves it
+        InventoryList inventoryList = new PlayerInventoryFile("PlayerInventory.csv");
+        inventoryList.initialize();
 
         // initialise a new fighting stats file and saves it
         FightingStatsInitializer fightingStatsInitializer = new FightingStatsInitializer();
