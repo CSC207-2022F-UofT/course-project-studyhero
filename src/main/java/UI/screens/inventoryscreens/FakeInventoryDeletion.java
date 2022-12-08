ce2f67c3bf7743bbe0cc664c6a2b0f49268f5700
Max Chen <maxlolm1k2l3@gmail.com>
2022-12-08 13:49:15 -0500

    Fixed FakeInventoryDeletion file

---
+++ src/main/java/UI/screens/inventoryscreens/FakeInventoryDeletion.java
@@ -0,0 +1,54 @@
+package UI.screens.inventoryscreens;
+
+
+
+import entities.inventoryitem.InventoryItemDsRequestModel;
+import use_cases.inventory_menu_use_case.delete_item_use_case.DeleteItemDsGateway;
+import use_cases.inventory_menu_use_case.delete_item_use_case.DeleteItemDsRequestModel;
+
+import java.util.ArrayList;
+
+public class FakeInventoryDeletion implements DeleteItemDsGateway {
+
+    public FakeInventoryDeletion(ArrayList<InventoryItemDsRequestModel> inventoryList) {
+        this.inventoryList = inventoryList;
+    }
+
+
+    private ArrayList<InventoryItemDsRequestModel> inventoryList;
+
+
+    /**
+     * Check if the item is in the inventory
+     * @return true if the item is in the inventory
+     */
+    @Override
+    public boolean itemExist(int id) {
+        return id <= inventoryList.size() && id > 0;
+    }
+
+    /**
+     * Delete the item from inventory
+     * @param id of the item wants to be removed from inventory
+     */
+    @Override
+    public void deleteItem(int id) {
+        inventoryList.remove(id - 1);
+        int newId = 1;
+        for(InventoryItemDsRequestModel item : inventoryList){
+            item.setId(newId);
+            newId++;
+        }
+    }
+
+    /**
+     * Get the name of the inventory item correspond to the id in the inventory
+     * @param id of the item wants to be removed from inventory
+     * @return the name of the item that wants to be removed from inventory
+     */
+    @Override
+    public DeleteItemDsRequestModel getDeletionName(int id) {
+
+        return new DeleteItemDsRequestModel(inventoryList.get(id - 1).getName());
+    }
+}
