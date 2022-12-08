45abeb9405516364e766630456a98b8bf8e1a66c
Max Chen <maxlolm1k2l3@gmail.com>
2022-12-08 13:07:44 -0500

    Updated the inventory item generator method

--- src/main/java/entities/inventoryitem/AddItemDsGateway.java
+++ src/main/java/entities/inventoryitem/AddItemDsGateway.java
@@ -1,4 +1,4 @@
-package entities.inventoryitem;
+package inventorymenu.inventoryitem;
 
 /**
 This interface obtain all the methods that will be implemented to add item to the inventory
@@ -10,6 +10,29 @@ public interface AddItemDsGateway {
       */
      boolean inventoryFull();
 
+     /**
+      * Generate a new weapon item based on player's level
+      */
+     void addNewWeapon(int level);
+
+     /**
+      * Generate a new shield in the inventory based on player's level
+      */
+     void addNewShield(int level);
+
+     /**
+      * Generate a new attack potion in the inventory based on player's level
+      */
+     void addNewAttackPotion(int level);
+     /**
+      * Generate a new health potion in the inventory based on player's level
+      */
+     void addNewHealthPotion(int level);
+
+     /**
+      * Generate a new poison potion in the inventory based on player's level
+      */
+     void addPoisonPotion(int level);
 
      /**
       * Add the inventoryItem to the inventoryList
