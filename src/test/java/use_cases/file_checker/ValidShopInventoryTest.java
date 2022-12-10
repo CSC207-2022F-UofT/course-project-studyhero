2412c82a67c3ff46e49fa0da1b3fbadc1981412b
Max Chen <maxlolm1k2l3@gmail.com>
2022-12-09 23:35:28 -0500

    Added the test for valid_player_inventory

---
+++ src/test/java/use_cases/file_checker/ValidShopInventoryTest.java
@@ -0,0 +1,28 @@
+package use_cases.file_checker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidShopInventoryTest {
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void checkError() {
+    }
+
+    @Test
+    void fileExists() {
+    }
+
+    @Test
+    void isPlayable() {
+    }
+
+    @Test
+    void isValid() {
+    }
+}
