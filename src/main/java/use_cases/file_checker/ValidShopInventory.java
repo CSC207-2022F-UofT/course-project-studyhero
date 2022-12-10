cd2921d16b27fb27564a9737e3bf375e6fe8f5d9
Max Chen <maxlolm1k2l3@gmail.com>
2022-12-10 12:34:10 -0500

    test headless exceptions

--- src/main/java/use_cases/file_checker/ValidShopInventory.java
+++ src/main/java/use_cases/file_checker/ValidShopInventory.java
@@ -147,38 +147,35 @@ public class ValidShopInventory implements ValidFileDsGateway{
         String result = checkError();
         if (result == null){return true;}
 
-        try{
-            switch(result){
-                case "exist":
-                    presenter.error("There is no existing " + filename + " file.");
-                    return false;
-                case "label":
-                case "header":
-                    presenter.error("Invalid " + filename + " file.");
-                    return false;
-                case "empty":
-                    presenter.error("There are no items in " + filename);
-                    return false;
-                case "index":
-                    presenter.error("Item IDs are invalid.");
-                    return false;
-                case "type":
-                    presenter.error("Items are of invalid types.");
-                    return false;
-                case "effect":
-                    presenter.error("Items have invalid effects.");
-                    return false;
-                case "goldValue":
-                    presenter.error("Items have invalid gold value.");
-                    return false;
-                case "other":
-                    presenter.error("Error: please start a new game.");
-                    return false;
-            }
-        }catch (HeadlessException e){
-            return false;
+        switch(result){
+            case "exist":
+                presenter.error("There is no existing " + filename + " file.");
+                return false;
+            case "label":
+            case "header":
+                presenter.error("Invalid " + filename + " file.");
+                return false;
+            case "empty":
+                presenter.error("There are no items in " + filename);
+                return false;
+            case "index":
+                presenter.error("Item IDs are invalid.");
+                return false;
+            case "type":
+                presenter.error("Items are of invalid types.");
+                return false;
+            case "effect":
+                presenter.error("Items have invalid effects.");
+                return false;
+            case "goldValue":
+                presenter.error("Items have invalid gold value.");
+                return false;
+            case "other":
+                presenter.error("Error: please start a new game.");
+                return false;
         }
-
+        try{presenter.error(presenter.getError()); }
+        catch (HeadlessException e){return false;}
         return false;
     }
 }
